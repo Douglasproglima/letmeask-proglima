fc6d5b2c5428de77be251c4faf144a9f5bf63814
Douglas Lima <douglasproglima@gmail.com>
2021-06-21 23:04:26 -0300

    New page Room and refatore Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Button } from './components/Button';
-import { Input } from './components/Input';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 
 function App() {
   return (
-    <Home />
+    <>
+      <BrowserRouter>
+        <Route path="/" exact component={ Home }/>
+        <Route path="/rooms/new" component={ NewRoom }/>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+
+import { useHistory } from 'react-router-dom';
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -5,26 +7,30 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home() {
+  const history = useHistory();
+
+  const navigateToNewRoom = () => history.push('/rooms/new');
+
   return (
     <div id="page-auth">
       <aside>
         <img src={ilustrationImg} alt="Ilustração perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
 
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask-proglima" />
-          <button className="create-room">
+          <button onClick={navigateToNewRoom} className="create-room" >
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">Ou entre em uma sala</div>
           <form action="">
             <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na Sala</Button>
           </form>
-          <Button type="submit">Entrar na Sala</Button>
         </div>
       </main>
 
